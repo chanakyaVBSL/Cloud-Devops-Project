e2c13a0261774839d4fdd871a138e84d3ea1e58a
Damian Heart <vbsl.chanakya6@gmail.com>
2025-04-04 10:16:04 +0530

    Change to V1

--- backend/models/Contact.js
+++ backend/models/Contact.js
@@ -3,12 +3,9 @@ const mongoose = require("mongoose");
 const contactSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
-    city: { type: String, required: true },
-    state: { type: String, required: true },
-    country: { type: String, required: true },
     phone: { type: String, required: true },
     message: { type: String, required: true },
     createdAt: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model("ContactV2", contactSchema);
+module.exports = mongoose.model("Contact", contactSchema);
